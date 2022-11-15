a0fb2415bb31a59091a6a4728ad571d5d578192c
huynh minh chu <huynhminhchu1999@gmail.com>
2022-11-15 13:57:21 +0700

    ok

--- quiz/main.go
+++ quiz/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"log"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func main() {
@@ -10,10 +15,49 @@ func main() {
 	if len(arguments) == 1 {
 		fmt.Println("only 1 argument")
 		return
-	} else {
-		for i:=1;i<len(arguments);i++ {
-			fmt.Println("index:", i, "value: ",arguments[i])
+	}
+	fileName := arguments[1]
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	r := csv.NewReader(file)
+	records, _ := r.ReadAll()
+
+	totalQuestion := 0
+	totalCorrectAns := 0
+	//fmt.Println(records)
+	for {
+		fmt.Println("Enter s to start the quiz game: ")
+		var start string
+		fmt.Scan(&start)
+		if strings.Compare(start, "s") == 0 {
+			break
 		}
 	}
-	
+	timer := time.NewTimer(30 * time.Second)
+	go func() {
+		<-timer.C
+		fmt.Println("Time out 30")
+	}()
+	for _, v := range records {
+		totalQuestion++
+		temp, _ := strconv.Atoi(v[1])
+		// re := regexp.MustCompile(`(?P<first>\d+)\+(?P<last>\d+)`)
+		// subStr := re.FindStringSubmatch(v[0])
+		// firstNum, _ := strconv.Atoi(subStr[1])
+		// secondNum, _ := strconv.Atoi(subStr[2])
+		// fmt.Println(firstNum, "+", secondNum)
+		fmt.Println(v[0])
+		fmt.Println("Please input your answer:")
+		var answer string
+		fmt.Scan(&answer)
+		temp2, _ := strconv.Atoi(answer)
+		if temp == temp2 {
+			totalCorrectAns++
+		}
+	}
+	fmt.Println("Total questions: ", totalQuestion)
+	fmt.Println("Total correct answers: ", totalCorrectAns)
 }
